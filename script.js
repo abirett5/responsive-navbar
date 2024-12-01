926429de194e75b712972a76045da969152c0412
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2024-12-01 15:19:05 +0600

    Updated

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const mobileNav = document.querySelector(".mobile-nav");
 
+// Mobile Nav Menu [Open]
 function openNav() {
     mobileNav.style.left = "0";
 }
 
+// Mobile Nav Menu [Close]
 function closeNav() {
     mobileNav.style.left = "-100%";
 }
